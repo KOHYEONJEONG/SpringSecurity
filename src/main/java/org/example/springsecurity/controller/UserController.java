366f82e12dd75ab4dbfbdebc62901d8e3caa1663
DESKTOP-275CC7F\EM_NB163 <hyeonjeong.ko@easymedia.net>
2024-11-01 10:12:35 +0900

    글로벌 예외 처리

---
+++ src/main/java/org/example/springsecurity/controller/UserController.java
@@ -0,0 +1,33 @@
+package org.example.springsecurity.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.example.springsecurity.anotation.TokenRequired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class UserController {
+    
+    @TokenRequired
+    @GetMapping("/user")
+    public String getUser(HttpServletRequest request, HttpServletResponse response) {
+            log.debug("getUser 호출");
+        return "user";
+    }
+
+    @TokenRequired
+    @GetMapping("/userDtl")
+    public String getUserDtl(@RequestParam("userId") String  userId, HttpServletRequest request, HttpServletResponse response) {
+        log.debug(userId+ " getUserDtl 호출");
+
+
+        //고의적으로 ArithmeticException 에러를 만들어보겠따!
+        int errorCode = 3 / 0;
+
+        return userId;
+    }
+}
